9fea82d7dfd0b6639b352275083c03eae49c10ba
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-09-19 19:14:04 +0100

    feat(chore): support Bind instances as input

--- src/bind.js
+++ src/bind.js
@@ -1,9 +1,5 @@
 (() => {
-  if (
-    typeof window === "undefined" ||
-    typeof document === "undefined" ||
-    !(document instanceof Document)
-  )
+  if (typeof window === "undefined" || typeof document === "undefined" || !(document instanceof Document))
     throw new Error("bind.js requires a DOM environment.");
 
   class BindError extends Error {
@@ -24,16 +20,15 @@
     #normalize(input) {
       if (input === undefined) return [];
 
-      if (typeof input === "string")
-        return [...document.querySelectorAll(input)];
+      if (typeof input === "string") return [...document.querySelectorAll(input)];
 
       if (input instanceof Element) return [input];
 
-      if (input instanceof NodeList || input instanceof HTMLCollection)
-        return [...input];
+      if (input instanceof NodeList || input instanceof HTMLCollection) return [...input];
 
-      if (Array.isArray(input))
-        return input.flatMap((item) => this.#normalize(item));
+      if (input instanceof Bind) return input.#elements;
+
+      if (Array.isArray(input)) return input.flatMap((item) => this.#normalize(item));
 
       throw new BindError(
         `Invalid input passed to bind(),\nbind.js accepts:\n\tstring\n\tElement\n\tNodeList\n\tHTMLCollection\n\tarray of supported inputs`,
